3e48f26c2ce4f79820d129531aa5210068f08d30
VanHai1424 <132867558+VanHai1424@users.noreply.github.com>
2024-03-07 11:08:17 +0700

    fix scroll lan cuoi

--- main.js
+++ main.js
@@ -183,11 +183,9 @@ const app = {
     },
     scrollToActiveSong: function() {
         setTimeout(() => {
-            const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-            const optionBlock = arr.includes(this.currentIndex) ? 'center' : 'nearest';
             $('.song.active').scrollIntoView({
                 behavior: 'smooth',
-                block: optionBlock
+                block: 'center'
             })
         }, 300);
     },
